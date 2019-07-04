7b1c66b481934451088303f4dcbff56b68bcc27d
Samara <al1609086@upy.edu.mx>
2019-07-04 05:05:28 -0500

    Final project: bot and code

---
+++ controllers/crazybot_munoz_samara/build/release/crazybot_munoz_samara.d
@@ -0,0 +1,7 @@
+build/release/crazybot_munoz_samara.o: crazybot_munoz_samara.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/position_sensor.h \
+ /usr/local/webots/include/controller/c/webots/keyboard.h
--- controllers/crazybot_munoz_samara/crazybot_munoz_samara.c
+++ controllers/crazybot_munoz_samara/crazybot_munoz_samara.c
@@ -9,13 +9,24 @@
 /*
  * You may need to add include files like <webots/distance_sensor.h> or
  * <webots/differential_wheels.h>, etc.
- */
+ */ ///LIBRERRIAS
 #include <webots/robot.h>
 #include <webots/motor.h>
+#include <webots/position_sensor.h>
+//#include <webots/distance_sensor.h>
+#include <webots/keyboard.h>
+
+///LIBRERIA STANDARDS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 /*
  * You may want to add macros here.
  */
 #define TIME_STEP 64
+double b=0;
+double e=0;
 
 /*
  * This is the main program.
@@ -26,6 +37,7 @@ int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
   wb_robot_init();
+  wb_keyboard_enable(TIME_STEP);
 
   /*
    * You should declare here WbDeviceTag variables for storing
@@ -33,19 +45,92 @@ int main(int argc, char **argv)
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
+ 
    
-   //esta importando
+  //esta importando
    WbDeviceTag wheel_left = wb_robot_get_device("motor_left");
-    WbDeviceTag wheel_right = wb_robot_get_device("motor_right
-    ");
+   WbDeviceTag wheel_right = wb_robot_get_device("motor_right");
+   //WbDeviceTag distance_sensor = wb_robot_get_device("distance_s");
+   WbDeviceTag right_pos = wb_robot_get_device("position_right");
+   WbDeviceTag left_pos = wb_robot_get_device("position_left"); 
+   
+   wb_position_sensor_enable(right_pos,TIME_STEP);
+   wb_position_sensor_enable(left_pos,TIME_STEP);
 
-
+ 
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
    * and leave the loop when the simulation is over
    */
   while (wb_robot_step(TIME_STEP) != -1) {
+     
+     
+     
 
+ double a,c,d,f;
+  
+  int key = wb_keyboard_get_key();
+  
+  wb_position_sensor_get_value(left_pos);
+  wb_position_sensor_get_value(right_pos);
+  
+  
+  a = wb_position_sensor_get_value(left_pos);
+  c = a-b ;
+  b = a;
+  
+  d = wb_position_sensor_get_value(right_pos);
+  f = d-e ;
+  e = d;
+  
+  
+  if(key==WB_KEYBOARD_UP){
+  
+    wb_motor_set_position(wheel_left, INFINITY);
+    wb_motor_set_velocity(wheel_left, -40);
+    wb_motor_set_position(wheel_right, INFINITY); 
+    wb_motor_set_velocity(wheel_right, -40); 
+    }
+    
+     if(key==WB_KEYBOARD_DOWN){
+  
+     wb_motor_set_position(wheel_left, INFINITY);
+     wb_motor_set_velocity(wheel_left, 1.33);
+     wb_motor_set_position(wheel_right, INFINITY); 
+      wb_motor_set_velocity(wheel_right, 1.33); 
+    }
+    
+    if(key==WB_KEYBOARD_LEFT){
+  
+     wb_motor_set_position(wheel_left, INFINITY);
+     wb_motor_set_velocity(wheel_left, -5);
+     wb_motor_set_position(wheel_right, INFINITY); 
+     wb_motor_set_velocity(wheel_right, -5); 
+    }
+    
+     if(key==WB_KEYBOARD_RIGHT){
+  
+     wb_motor_set_position(wheel_left, INFINITY);
+     wb_motor_set_velocity(wheel_left, 5);
+     wb_motor_set_position(wheel_right, INFINITY); 
+     wb_motor_set_velocity(wheel_right, 5); 
+    }
+    
+    
+    
+    
+     printf("Angular Velocity Left: %lf  \n", (c/0.064));
+     printf("Linear Velocity Left: %lf  \n", (c/0.064)*0.075);
+     printf("Left RPM: %f \n", (c/0.064)*(60/(2*3.1416)));
+     
+     printf("Angular Velocity Right: %lf  \t", (f/0.064));
+     printf("Linear Velocity Right: %lf  \t", (f/0.064)*0.075);
+     printf("Right RPM: %f \t", (f/0.064)*(60/(2*3.1416)));
+     
+      fflush(stdout);
+    
+    
+     
     /*
      * Read the sensors :
      * Enter here functions to read sensor data, like:
